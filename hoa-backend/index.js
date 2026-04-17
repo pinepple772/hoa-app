bcdad3d8ab7dcbab9ef65f130a3e853d7a6d2323
pinepple772 <pineapple.bacon123@gmail.com>
2026-04-16 23:52:56 -0500

    modified index.js to read FRONTEND_URL from .env

--- hoa-backend/index.js
+++ hoa-backend/index.js
@@ -11,9 +11,8 @@ const openai = new OpenAI({
 const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
-  process.env.hoa-frontend-beta.vercel.app
+  process.env.FRONTEND_URL 
 ].filter(Boolean);
-
 app.use(cors({ 
   origin: allowedOrigins,
   credentials: true
